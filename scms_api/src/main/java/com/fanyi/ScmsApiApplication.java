4746c47047fe2bf5d6f365052113653af48f9222
wangyds <wangyds@yonyou.com>
2019-05-26 20:05:34 +0800

    更改pom的配置

--- scms_api/src/main/java/com/fanyi/ScmsApiApplication.java
+++ scms_api/src/main/java/com/fanyi/ScmsApiApplication.java
@@ -3,6 +3,10 @@ package com.fanyi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wangyds
+ * @date 2019/05/26
+ */
 @SpringBootApplication
 public class ScmsApiApplication {
 
